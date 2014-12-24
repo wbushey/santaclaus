ed1fecccc0615ce909267fc7c294d3e8f12a0ee1
Bill Bushey <wbushey@gmail.com>
2014-12-23 20:02:11 -0600

    Testing the Person model a bit Using Faker in tests

--- tests/test_santa.py
+++ tests/test_santa.py
@@ -1,10 +1,13 @@
 # -*- coding: utf8 -*-
 from __future__ import absolute_import, unicode_literals
+from faker import Faker
 import json
 import os
 import tempfile
 import unittest
 from santaclaus import app, db
+from santaclaus.models import Person
+
 
 class SantaClausTest(unittest.TestCase):
 
@@ -13,6 +16,7 @@ class SantaClausTest(unittest.TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
         db.create_all()
+        self.fake = Faker()
 
     def tearDown(self):
         pass
@@ -22,8 +26,8 @@ class SantaClausTest(unittest.TestCase):
     def assert_json_response(self, r):
         self.assertEqual(r.headers['Content-Type'], 'application/json')
 
-    def test_with_a_name(self):
-        r = self.app.get('/?name=Somebody')
+    def test_request_with_a_name(self):
+        r = self.app.get('/?name=%s' % self.fake.name().replace(" ", "%20"))
 
         self.assertEqual(r.status_code, 200)
         self.assert_json_response(r)
@@ -31,10 +35,24 @@ class SantaClausTest(unittest.TestCase):
         self.assertTrue('name' in data_js)
         self.assertTrue('status' in data_js)
 
-    def test_without_a_name(self):
+    def test_request_without_a_name(self):
         r = self.app.get('/')
 
         self.assertEqual(r.status_code, 400)
         self.assert_json_response(r)
         data_js = json.loads(r.data)
         self.assertTrue('error' in data_js)
+
+    def test_person_model_decides_status(self):
+        p = Person(self.fake.name())
+        self.assertTrue(p.status in ['Naughty', 'Nice'])
+
+    def test_status_ratio(self):
+        naughties = 0
+        for i in range(100):
+            p = Person(self.fake.name())
+            if p.status == 'Naughty':
+                naughties = naughties + 1
+
+        # Computer randomness is far from perfect, so let's test in a range
+        self.assertTrue(naughties > 40 and naughties < 60)
